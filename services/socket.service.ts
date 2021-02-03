c658f21b61ad6727d0c06786961293cb2b38b3fc
Anton Kurdo <anton.kurdo@gmail.com>
2021-02-03 12:56:48 +0300

    update socketStart

--- services/socket.service.ts
+++ services/socket.service.ts
@@ -2,12 +2,21 @@ export {};
 const Chat = require('../models/chat.model');
 const Message = require('../models/message.model');
 
+interface iMessage {
+  _id: string,
+  date: Date,
+  authorId: string,
+  authorFullName: string,
+  content: string,
+  chatId: string
+};
+
 const socketStart = async (io: any) => {
   try {
     io.on("connection", (socket: any) => {        
       socket.on("chat message", async (msg: {content: string, authorFullName?: string}, fromId: string, toId: string) => {       
         const chat = await Chat.findOne({$or: [{_id: `${fromId}${toId}`}, {_id: `${toId}${fromId}`}]});  
-        let messages: any = [];    
+        let messages: Array<iMessage> = [];    
         if(!chat) {
           const newChat = new Chat({
             _id: `${fromId}${toId}`,
@@ -16,30 +25,28 @@ const socketStart = async (io: any) => {
           await newChat.save()
         }        
         if(msg.content !== 'start') {     
-           const {authorFullName, content} = msg;
-           const newMessage = new Message({
+          const {authorFullName, content} = msg;
+          const newMessage = new Message({
             authorId: fromId,
             authorFullName,
             content,
             chatId: chat.id || `${fromId}${toId}`
-          })
+          });
           await newMessage.save();
           const msgs = chat 
                           ? await Message.find({chatId: chat.id})
                           : await Message.find({chatId: `${fromId}${toId}`})
-          messages = [...messages, ...msgs]
+          messages = [...messages, ...msgs];
           io.emit("chat message", JSON.stringify(messages));
         } else {  
           const msgs = chat 
                         ? await Message.find({chatId: chat.id})
                         : await Message.find({chatId: `${fromId}${toId}`})
-          messages = [...messages, ...msgs]
+          messages = [...messages, ...msgs];
           io.emit("chat message", JSON.stringify(messages));
-        }
-     
+        }     
       });   
     });
-
   } catch(e) {
     console.log(e)
   }
