1e19be468e434b44b8a3f6208d17dda0acd7490e
AntonKurdo <anton.kurdo@gmail.com>
2021-03-04 18:19:44 +0300

    add history of authorizations in firebase

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,18 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import MainNavigator from './src/navigation/Main.nav';
 import {AppState} from './src/context/State';
 import { MenuProvider } from 'react-native-popup-menu';
+import firebase from "firebase";
+import { firebaseConfig } from './firebaseConfig';
+
 
 const App: FC = () => {
+    useEffect(() => {
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }      
+    }, [])
+
     return (
         <AppState >
             <MenuProvider>
---
+++ client/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD5voLn485Ega8mg5C3rciF_ZCNy4GOmc0",
+  authDomain: "final-project-vironit.firebaseapp.com",
+  databaseURL: "https://final-project-vironit-default-rtdb.firebaseio.com",
+  projectId: "final-project-vironit",
+  storageBucket: "final-project-vironit.appspot.com",
+  messagingSenderId: "632259900656",
+  appId: "1:632259900656:web:160c2dda5f18b6c3983e35"
+};
