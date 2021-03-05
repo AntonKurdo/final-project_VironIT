edd4bce21a8707b32e7ef3088e90a69e509c1cfd
AntonKurdo <anton.kurdo@gmail.com>
2021-03-05 18:22:07 +0300

    add notifications

--- client/App.tsx
+++ client/App.tsx
@@ -1,17 +1,22 @@
 import React, {FC, useEffect} from 'react';
+import { LogBox, Platform } from 'react-native';
 import MainNavigator from './src/navigation/Main.nav';
 import {AppState} from './src/context/State';
 import { MenuProvider } from 'react-native-popup-menu';
 import firebase from "firebase";
 import { firebaseConfig } from './firebaseConfig';
+import { regusterForPushNotifications } from './src/services/getNotificationsPermission.service';
 
+Platform.OS === 'android' && LogBox.ignoreLogs(['Setting a timer']);
 
 const App: FC = () => {
+
     useEffect(() => {
         if (!firebase.apps.length) {
             firebase.initializeApp(firebaseConfig);
-        }      
-    }, [])
+        }     
+        regusterForPushNotifications();          
+    }, []) 
 
     return (
         <AppState >
---
+++ client/src/services/getNotificationsPermission.service.ts
@@ -0,0 +1,25 @@
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+
+export const regusterForPushNotifications  = async () => {       
+  const { status } = await  Permissions.getAsync(Permissions.NOTIFICATIONS);
+  let finalStatus = status;
+  if(status !== 'granted') {
+      const { status } = await  Permissions.getAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+  } 
+  if(finalStatus !== 'granted') { return; }
+  let token = await Notifications.getExpoPushTokenAsync();
+  console.log(token);
+  Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+      }),
+    });
+  Notifications.addNotificationResponseReceivedListener((event) => {
+      console.log('open')
+     
+  }) 
+};
